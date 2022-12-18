93af6e121c367c96bb1f0ee9dbe28a8f53bed08e
ImAClownAndDontComplain <94308517+ImAClownAndDontComplain@users.noreply.github.com>
2022-12-18 14:14:45 +0300

    рабочие страницы

--- web-app/helpme/main/urls.py
+++ web-app/helpme/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('about', views.about),
-    path('result', views.result)
+    path('', views.index, name = 'index'),
+    path('about', views.about, name = 'about'),
+    path('result', views.result, name = 'result')
 ]
